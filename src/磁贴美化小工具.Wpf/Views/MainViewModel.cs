46395d27aa0e54c7b16b3b2601eaaeed5ee89059
walterlv <xblero@gmail.com>
2020-10-18 12:00:50 +0800

    添加导航页面

---
+++ src/磁贴美化小工具.Wpf/Views/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xiu2.TileTool.UIFramework;
+
+namespace Xiu2.TileTool.Views
+{
+    public class MainViewModel : BindableObject
+    {
+        public ObservableCollection<NavigationItem> NavigationItems { get; } = new ObservableCollection<NavigationItem>
+        {
+            NavigationItem.Combine<EditTilePage, EditTileViewModel>("新建/编辑磁贴", null, ""),
+            NavigationItem.Combine<AboutPage, AboutViewModel>("关于", null, ""),
+            NavigationItem.Combine<SettingsPage, SettingsViewModel>("设置", null, ""),
+        };
+    }
+}
